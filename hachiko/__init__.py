20680cebe98d47401cb7b44b35e8eff3d72a7caf
Tuomo <tuokri@tuta.io>
2025-01-12 20:32:36 +0200

    Modernize

--- hachiko/__init__.py
+++ hachiko/__init__.py
@@ -1,1 +1,4 @@
-from .version import __version__, __version_info__
+from .hachiko import AIOEventHandler
+from .hachiko import AIOWatchdog
+from .version import __version__
+from .version import __version_tuple__
